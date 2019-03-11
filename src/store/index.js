3bf361c2a8e243fce4028b667ee604872f1737dd
kuma <test@nihonn.net>
2019-03-11 14:45:51 +0800

    refactor: 重构Vuex模块,并为后面提示消息组件做数据管理.

---
+++ src/store/index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Import from '_js/import';
+
+const ModuleFiles = require.context('./modules', true, /(?<!index)\.js$/),
+    modules = Import(ModuleFiles, false);
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        // 导航标签
+        tabs: [],
+        // 提示消息
+        alertMessage: [],
+    },
+    getters: {
+        alertMessage: state => state.alertMessage,
+        tabs: state => state.tabs,
+    },
+    mutations: {
+        alertMessagePush(state, data) {
+            state.alertMessage.push(data);
+        },
+        alertMessageDel(state, index) {
+            state.alertMessage.splice(index, 1);
+        },
+    },
+    actions: {
+        alertMessage({ getters, commit }, data) {
+            if (typeof data === 'string') data = { content: data };
+            let msg = {
+                    top: true,
+                    color: 'info',
+                    timeout: 2000,
+                    content: '',
+                    ...data,
+                },
+                length = getters.alertMessage.length;
+            commit('alertMessagePush', msg);
+            if (msg.timeout !== 0) {
+                setTimeout(() => {
+                    commit('alertMessageDel', length - 1);
+                }, msg.timeout);
+            }
+        },
+    },
+    modules,
+});
---
+++ src/store/modules/storeUser.js
@@ -0,0 +1,11 @@
+let state = {};
+const getters = {},
+    mutations = {},
+    actions = {};
+export default {
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions,
+};
