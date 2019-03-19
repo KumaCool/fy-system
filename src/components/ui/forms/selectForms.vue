70d637a9f82b9ce393cfb915bcd5dfee8af8a462
kuma <test@nihonn.net>
2019-03-19 15:57:44 +0800

    feat: 增加查询表单组件.

---
+++ src/components/ui/forms/selectForms.vue
@@ -0,0 +1,47 @@
+<script>
+import { createHOC } from 'vue-hoc';
+import AutoForms from '~/ui/forms';
+
+export default createHOC(AutoForms, { name: 'SelectForms' }, {
+    'class': { selectForms: true },
+    props: {
+        container(props) {
+            let data = props.container || {};
+            return R.mergeDeepRight({
+                attrs: {
+                    'px-4': true,
+                    'py-2': true,
+                },
+            }, data);
+        },
+        formOption(props) {
+            let data = props.formOption || {};
+            return R.mergeDeepRight({ props: { outline: true } }, data);
+        },
+        layout(props) {
+            let data = props.layout || {};
+            return R.mergeDeepRight({ attrs: { wrap: true } }, data);
+        },
+        flex(props) {
+            let data = props.flex || {};
+            return R.mergeDeepRight({ attrs: { shrink: true } }, data);
+        },
+    },
+});
+</script>
+
+<style lang="stylus" scoped>
+.selectForms >>>
+    background $_color_white
+    border-left 9px solid $_color_primary
+    radius()
+    shadow(0 2px 4px)
+    .container .formItem
+        .label
+            // margin-bottom 25px
+            font-weight bold
+        .v-input__slot
+            margin 0
+        .v-text-field__details
+            display none
+</style>
