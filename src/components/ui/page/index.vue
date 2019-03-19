e25a4d81e034ebac5f346bfd2f53ccb3c5a2232a
kuma <test@nihonn.net>
2019-03-19 15:21:07 +0800

    feat: 增加翻页组件.

---
+++ src/components/ui/page/index.vue
@@ -0,0 +1,32 @@
+<script>
+import { createHOC } from 'vue-hoc';
+import { VPagination } from 'vuetify/lib';
+export default createHOC(VPagination, {
+    name: 'autoPage',
+    render(h) {
+        let com = h(VPagination, {
+            props: this.$props,
+            attrs: this.$attrs,
+            on: this.$listeners,
+            scopedSlots: this.$scopedSlots,
+        });
+        return (<div class="page">{com}</div>);
+    },
+});
+</script>
+
+<style lang="stylus" scoped>
+.page >>>
+    margin 0 auto
+    text-align center
+    .v-pagination
+        padding 5px
+        shadow()
+        background $_color_white
+        .v-pagination__navigation
+        .v-pagination__item
+            size(28px)
+            min-width 28px
+            background $_color_background_page
+            shadow(none)
+</style>
