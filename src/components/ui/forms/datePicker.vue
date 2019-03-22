5131839be23cdb32229f2bb3c303ad8b0b273688
kuma <test@nihonn.net>
2019-03-22 14:15:24 +0800

    feat: 扩展日期选择器功能.

--- src/components/ui/forms/datePicker.vue
+++ src/components/ui/forms/datePicker.vue
@@ -1,9 +1,10 @@
 <template lang="html">
     <v-menu>
         <template v-slot:activator="{on}">
-            <v-text-field v-bind="inputAttrs"
+            <v-text-field v-bind="inputAttrsObj"
                           :value="value"
-                          v-on="{...on, ...inputListeners}" />
+                          v-on="{...on, ...inputListeners}"
+                          @input="change" />
         </template>
         <v-date-picker v-bind="attrs"
                        v-on="$listeners" />
@@ -18,6 +19,8 @@ export default {
             type: String,
             required: true,
         },
+        // 清空按钮
+        clearable: Boolean,
         // 文本框属性配置
         inputAttrs: {
             type: Object,
@@ -34,13 +37,23 @@ export default {
         },
     },
     computed: {
+        // 文本框属性配置
+        inputAttrsObj() {
+            let attrs = { clearable: this.clearable };
+            return R.mergeDeepLeft(this.inputAttrs, attrs);
+        },
         // 选择器属性
         attrs() {
             let attrs = {
                 'no-title': true,
                 locale: 'zh-cn',
             };
-            return  R.mergeDeepLeft(this.$attrs, attrs);
+            return R.mergeDeepLeft(this.$attrs, attrs);
+        },
+    },
+    methods: {
+        change(v) {
+            this.$emit('input', v || '');
         },
     },
 };
