0e9336fd649e9cd0c5f6955e8f2a46dc4afc67cc
kuma <test@nihonn.net>
2019-03-25 20:47:31 +0800

    style: 变更项目全局样式.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import '@/plugins/vuetify';
-import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
 import Components from '~/';
 import '_css/global.styl';
+import App from '@/App.vue';
 
 Vue.config.productionTip = false;
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,5 +1,16 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import 'vuetify/src/stylus/app.styl';
+import '_css/vuetify.styl';
 
-Vue.use(Vuetify, { iconfont: 'md' });
+Vue.use(Vuetify, {
+    theme: {
+        primary: '#A36CC7',
+        secondary: '#048BF4',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107',
+    },
+    iconfont: 'md',
+});
