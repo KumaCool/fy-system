e3d9cac2822c4cc6e681b271cbcb7dc0e19d2bf7
kuma <test@nihonn.net>
2019-03-25 20:24:09 +0800

    feat: 添加下载组件.

---
+++ src/components/ui/forms/upload.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+    <v-btn v-bind="$attrs"
+           v-on="$listeners"
+           @click="openFind">
+        <slot />
+        <input ref="fileInput"
+               type="file"
+               hidden="hidden"
+               @change="change">
+    </v-btn>
+</template>
+
+<script>
+export default {
+    name: 'Upload',
+    methods: {
+        // 打开文件选择器
+        openFind() {
+            this.$refs.fileInput.click();
+        },
+        change(e) {
+            let files = e.target.files,
+                data = new FormData();
+            data.append('file', files[0]);
+            this.$emit('change', data);
+        },
+    },
+};
+</script>
