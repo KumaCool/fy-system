25c4a0d22d69463f5793c83800ec9fdcb15d1c46
kuma <test@nihonn.net>
2019-04-01 10:39:28 +0800

    refactor: 优化列表型表格组件状态提示.

--- src/components/ui/table/index.vue
+++ src/components/ui/table/index.vue
@@ -1,7 +1,15 @@
 <script>
 import { createHOC } from 'vue-hoc';
 import { VDataTable } from 'vuetify/lib';
-export default createHOC(VDataTable, { name: 'ListTable' }, { 'class': { listTable: true } });
+export default createHOC(VDataTable, { name: 'ListTable' }, {
+    'class': { listTable: true },
+    props: {
+        'noDataText'(props) {
+            if (this.$attrs.loading) return '加载数据中...';
+            return props.noDataText || '暂无数据';
+        },
+    },
+});
 </script>
 
 <style lang="stylus" scoped>
