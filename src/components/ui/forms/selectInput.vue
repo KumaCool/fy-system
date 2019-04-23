a228f898683f1e77008ec01b09ab16d6dabf56d5
kuma <test@nihonn.net>
2019-04-23 10:02:47 +0800

    feat: 增加搜索文本框组件.

---
+++ src/components/ui/forms/selectInput.vue
@@ -0,0 +1,29 @@
+<script>
+import Vue from 'vue';
+import { createHOC } from 'vue-hoc';
+import { VTextField, VBtn } from 'vuetify/lib';
+Vue.component('v-btn', VBtn);
+export default createHOC(VTextField, {
+    methods: {
+        select() {
+            this.$emit('select');
+        },
+    },
+}, {
+    slots: {
+        append(slots) {
+            if (!slots.append) {
+                slots.append = [<v-btn color="warning" small onClick={this.select}>搜索</v-btn>];
+            }
+        },
+    },
+    listeners: {
+        keyup(e) {
+            if (e.keyCode === 13) this.select();
+        },
+    },
+});
+</script>
+
+<style lang="css" scoped>
+</style>
